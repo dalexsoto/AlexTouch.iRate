db22cf953116281f3de3d178a6626f30b10dac59
Alex Soto <dxdream@gmail.com>
2012-10-15 01:32:07 -0500

    Updated to Version 1.6.1

--- Bindings/AlexTouch.iRate/AlexTouch.iRate/libiRate.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("libiRate.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, Frameworks = "UIkit")]
